71e0f8fc3af52c5b7e3dc7d55e81c9c68411b522
vuppeti <saiprakashreddy411@gmail.com>
2024-06-14 13:06:38 -0400

    efwefef

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
+// Import the 'act' function from the 'react' package
+import { act } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  // Use the 'act' function instead of 'ReactDOMTestUtils.act'
+  act(() => {
+    render(<App />);
+  });
+
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
