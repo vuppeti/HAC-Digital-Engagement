294b59ce433a647cc4a6a4a3297172d079bd4ad8
vuppeti <saiprakashreddy411@gmail.com>
2024-06-14 13:26:59 -0400

    bgfbgf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import AlpaNavTermlist from './components/AlpaNavTermlist.js';
 import Header from './components/Header.js';
